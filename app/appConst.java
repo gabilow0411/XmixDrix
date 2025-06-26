f0d44c80a446d1890d0404dc2ab5933685f1606c
gabilow0411 <76735016+gabilow0411@users.noreply.github.com>
2025-06-26 14:26:03 +0300

    Update StartActivity.java

--- app/appConst.java
+++
@@ -1,5 +0,0 @@
-public class appConst {
-    public    int Rows=0;
-    public  int Colums=3;
-    public  int Empty=0;
-}
--- app/src/main/java/gabi/low/class2ex/Basics.java
+++
@@ -1,8 +0,0 @@
-package gabi.low.class2ex;
-
-public class Basics {
-    public static int ROWS =3;
-    public  static int COLUMNS =3;
-    public  static int EMPTY =0;
-
-}
